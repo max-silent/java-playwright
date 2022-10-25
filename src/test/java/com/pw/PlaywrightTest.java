eb21381ebff51d4101930f68f601051d457d66b6
Max <mirotvoretz@gmail.com>
2022-10-25 21:27:17 +0300

    Added monitorHttpTraffic test.

--- src/test/java/com/pw/PlaywrightTest.java
+++ src/test/java/com/pw/PlaywrightTest.java
@@ -5,12 +5,14 @@ import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PlaywrightTest {
 
@@ -66,5 +68,30 @@ public class PlaywrightTest {
             System.out.println(details.first().innerText());
             page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get(channel + ".png")));
         }
+    }
+
+    @Test
+    public void monitorHttpTraffic() {
+        Page page = Playwright.create().chromium().launch().newPage();
+//        page.onRequest(req -> System.out.println(">>> " + req.method() + ": " + req.url()));
+//        page.onResponse(resp -> {
+//            System.out.println("<<< " + resp.status() + ":");
+//            System.out.println(resp.text());
+//        });
+
+// Approach 1
+        List<Integer> statuses = new ArrayList<>();
+        page.onResponse(resp -> statuses.add(resp.status()));
+        page.navigate("https://playwright.dev/");
+        System.out.println(statuses);
+        boolean unexpectedStatus = statuses.stream().anyMatch(status -> status < 200 || status >= 300);
+        Assertions.assertFalse(unexpectedStatus);
+
+// Approach 2
+        List<Boolean> results = new ArrayList<>();
+        page.onResponse(resp -> results.add(resp.ok()));
+        page.navigate("https://playwright.dev/");
+        System.out.println(results);
+        Assertions.assertTrue(results.stream().allMatch(result -> result));
     }
 }
